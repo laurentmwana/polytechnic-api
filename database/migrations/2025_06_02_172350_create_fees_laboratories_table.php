5d3ddf32f3a8669299653212941f6792fa8c2dcc
Labeya <laurentmwn@gmail.com>
2025-06-02 18:40:58 +0100

    create migration for paid laboratory and academic fees

---
+++ database/migrations/2025_06_02_172350_create_fees_laboratories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fees_laboratories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('year_academic_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('level_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fees_laboratories');
+    }
+};
