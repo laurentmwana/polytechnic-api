7d2ce18faae2718c1b07afc2e67a13a662e6faed
Labeya <laurentmwn@gmail.com>
2025-06-27 22:42:48 +0100

    crud admin actuality

---
+++ app/Http/Resources/Actuality/ActualityCollectionResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Actuality;
+
+use App\Http\Resources\Comment\CommentItemResource;
+use App\Http\Resources\Like\LikeActionResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ActualityCollectionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'likes' => LikeActionResource::collection($this->likes),
+            'comments' => CommentItemResource::collection($this->comments),
+            'created_at' => $this->created_at,
+        ];
+    }
+}
