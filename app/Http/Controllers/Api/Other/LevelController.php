2eff9eddcac468867398c524527178d489ea6c5e
Labeya <laurentmwn@gmail.com>
2025-06-27 11:21:44 +0100

    search and filter level by semester

--- app/Http/Controllers/Api/Other/LevelController.php
+++ app/Http/Controllers/Api/Other/LevelController.php
@@ -6,14 +6,45 @@ use App\Models\Level;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Level\LevelItemResource;
 use App\Http\Resources\Level\LevelCollectionResource;
+use App\Services\SearchData;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 
 class LevelController extends Controller
 {
-    public function index()
+    private const SEARCH_FIELDS_LEVEL = [
+        'name',
+        'id',
+        'alias',
+        'created_at',
+        'updated_at',
+    ];
+
+    private const SEARCH_FIELDS_DEPARTMENT = [
+        'name',
+        'id',
+        'alias',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function index(Request $request)
     {
-        $levels = Level::with(['department'])
-            ->orderByDesc('updated_at')
-            ->paginate();
+        $builder = Level::with(['department'])
+            ->orderByDesc('updated_at');
+
+        $search = $request->query->get('search');
+
+        if (!empty($search)) {
+            $builder->where(function (Builder $query) use ($search) {
+                SearchData::handle($query, $search, self::SEARCH_FIELDS_LEVEL);
+                 $query->orWhereHas('yearAcademic', function ($q) use ($search) {
+                    SearchData::handle($q, $search, self::SEARCH_FIELDS_DEPARTMENT);
+                });
+            });
+        }
+
+        $levels = $builder->paginate();
 
         return LevelCollectionResource::collection($levels);
     }
