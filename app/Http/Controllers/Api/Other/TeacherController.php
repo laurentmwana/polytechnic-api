c84dc9020dec20f09e71c7a169dd3640a0156e92
Labeya <laurentmwn@gmail.com>
2025-06-27 11:22:17 +0100

    search teacher

--- app/Http/Controllers/Api/Other/TeacherController.php
+++ app/Http/Controllers/Api/Other/TeacherController.php
@@ -6,16 +6,45 @@ use App\Models\Teacher;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Teacher\TeacherItemResource;
 use App\Http\Resources\Teacher\TeacherCollectionResource;
+use App\Services\SearchData;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
+    private const SEARCH_FIELDS_TEACHER = [
+        'name',
+        'id',
+        'firstname',
+        'gender',
+        'created_at',
+        'updated_at',
+    ];
+
+    private const SEARCH_FIELDS_DEPARTMENT = [
+        'name',
+        'id',
+        'alias',
+        'created_at',
+        'updated_at',
+    ];
+
     public function index(Request $request)
     {
+        $search = $request->query->get('search');
         $limit = $request->query->getInt('limit');
 
         $builder = Teacher::with(['department', 'courses'])
             ->orderByDesc('updated_at');
+
+        if (!empty($search)) {
+            $builder->where(function (Builder $query) use ($search) {
+                SearchData::handle($query, $search, self::SEARCH_FIELDS_TEACHER);
+                 $query->orWhereHas('yearAcademic', function ($q) use ($search) {
+                    SearchData::handle($q, $search, self::SEARCH_FIELDS_DEPARTMENT);
+                });
+            });
+        }
 
         $teachers = $limit > 0 ? $builder->limit($limit)->get() : $builder->paginate();
 
